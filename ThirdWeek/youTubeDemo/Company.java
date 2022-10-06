fb9c0f61cec9b1e617cd28ba775f8037fc2e1f5c
ZaferCanYorulmaz <114951491+Zafer-Can-Y@users.noreply.github.com>
2022-10-06 23:31:27 +0300

    ThirdWeek

---
+++ ThirdWeek/youTubeDemo/Company.java
@@ -0,0 +1,23 @@
+package youTubeDemo;
+
+public class Company extends Customer {
+	private String taxNumber;
+	private String companyName;
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getTaxNumber() {
+		return taxNumber;
+	}
+
+	public void setTaxNumber(String taxNumber) {
+		this.taxNumber = taxNumber;
+	}
+
+}

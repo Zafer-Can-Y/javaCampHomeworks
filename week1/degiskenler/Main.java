b2104089a640000d121adb74898cd12cf43f9849
ZaferCanYorulmaz <114951491+Zafer-Can-Y@users.noreply.github.com>
2022-10-28 22:41:27 +0300

    week1

---
+++ week1/degiskenler/Main.java
@@ -0,0 +1,12 @@
+package degiskenler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
---
+++ week1/miniProjeAsalSayi/Main.java
@@ -0,0 +1,31 @@
+package miniProjeAsalSayı;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 1;
+		int reminder = number % 2;
+		// System.out.println(reminder);
+		boolean isPrime = true;
+
+		if (number == 1) {
+			System.out.println("Sayı asal değildir");
+			return;
+		}
+		if (number < 1) {
+			System.out.println("Geçersiz sayı");
+		}
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0) {
+				isPrime = false;
+			}
+		}
+		if (isPrime == true) {
+			System.out.println("Sayı asaldır.");
+		} else {
+			System.out.println("Sayı asal değildir.");
+		}
+
+	}
+
+}
